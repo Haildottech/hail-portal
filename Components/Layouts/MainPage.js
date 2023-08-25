07af6f4d88c44547d6a924454aba2b9b6381cd15
Haildottech <haildottech@gmail.com>
2023-08-25 06:25:21 -0700

    update

--- Components/Layouts/MainPage.js
+++ Components/Layouts/MainPage.js
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 import React, { useEffect } from 'react';
 import Router from 'next/router';
 import { delay } from '@/functions/delay';
--- Components/Shared/MainLayout.js
+++ Components/Shared/MainLayout.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { UploadOutlined, CloseCircleOutlined, FileDoneOutlined } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
